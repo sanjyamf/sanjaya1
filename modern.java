48e9b2dafa5a535925c37a51f30b41cc3d25eacb
sanjyamf <32727687+sanjyamf@users.noreply.github.com>
2017-10-18 19:46:36 +0700

    Add files via upload

---
+++ modern.java
@@ -0,0 +1,19 @@
+public class modern{ // Nama Class 
+public static void main(String [] args){ // Main Method 
+System.out.println( "______________________________ " );
+System.out.println( " cara memasak nasi  modern:" );
+System.out.println( "______________________________" );
+System.out.println( " ==================================================================== " );
+System.out.println( " 1.ukur beras dengan cangkir takar. " );
+System.out.println( " 2.Saat kita mulai membersihkan berasnya,   " ); 
+System.out.println( " 3.ukur takaran air.  . " ); 
+System.out.println( " 4.Rendam beras selama tiga puluh menit kalu suka  " );
+System.out.println( " 5.Tambahkan perasa (sesuai selera) " );  
+System.out.println( " 6.masukan kedalam rice cooker  " );  
+System.out.println( " 7.atur rice coooker. " );   
+System.out.println( " 8.tunggu hingga poroses matang  . " );
+System.out.println( "======================================================================== " ); // Inputan
+
+
+}
+}
